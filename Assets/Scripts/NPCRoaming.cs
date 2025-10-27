fa266a08b329e521cce128ac81a20086932452a0
WDGH-TL <williamdantegarcia@gmail.com>
2025-10-27 20:19:58 -0300

    Scenario & NavMesh
    NPC Movement(NavMesh) Done
    Scenario Aesthetics(no lighting)

---
+++ Assets/Scripts/NPCRoaming.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCRoaming : MonoBehaviour
+{
+    public NavMeshAgent characterMoving;
+    public Transform[] walkingPoints;
+    public float distanceDestinies;
+    private int destinies;
+    public float idleTimer;
+    public float walkingSpeed = 5.5f;
+    public float idleTime = 2.0f;
+
+    public enum NPC_STATE
+    {
+        Idle,
+        Walking
+    }
+    public NPC_STATE currentState;
+
+    void Start()
+    {
+        currentState = NPC_STATE.Idle;
+        distanceDestinies = Vector3.Distance(transform.position, walkingPoints[destinies].position);
+        characterMoving.destination = walkingPoints[destinies].position;
+    }
+
+    public void Update()
+    {
+        switch(currentState)
+        {
+            case NPC_STATE.Idle:
+                idleTimer -= Time.deltaTime;
+                if (idleTimer <= 0)
+                {
+                    destinies = (destinies + 1) % walkingPoints.Length;
+                    WalkingState();
+                }
+                break;
+
+            case NPC_STATE.Walking:
+                if (!characterMoving.pathPending && characterMoving.remainingDistance < 0.5f)
+                {
+                    IdleState();
+                }
+                break;
+        }
+    }
+    void IdleState()
+    {
+        currentState = NPC_STATE.Idle;
+        idleTimer = idleTime;
+        characterMoving.isStopped = true;
+    }
+    void WalkingState()
+    {
+        currentState = NPC_STATE.Walking;
+        characterMoving.isStopped = false;
+        characterMoving.speed = walkingSpeed;
+        characterMoving.destination = walkingPoints[destinies].position;
+    }
+}
