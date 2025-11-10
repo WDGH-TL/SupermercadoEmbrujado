e475621ab50e8e21ff822af21dc01250fefeae62
WDGH-TL <williamdantegarcia@gmail.com>
2025-11-09 23:05:53 -0300

    Voucher
    Voucher is ready
    Need more products

--- Assets/Scripts/Voucher.cs
+++ Assets/Scripts/Voucher.cs
@@ -1,6 +1,67 @@
 using UnityEngine;
+using TMPro;
 
 public class Voucher : MonoBehaviour
 {
-    //En el inventario interno llamar al voucher
+
+    public GameObject voucher;
+    public TextMeshProUGUI inventoryDisplay;
+    public TextMeshProUGUI total;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            if (voucher != null)
+            {
+                voucher.SetActive(true);
+                string displayText = "\n";
+                float totalValue = 0f;
+
+
+                for (int i = 0; i < Inventory.Instance.itemInventory.Length; i++)
+                {
+                    Items item = Inventory.Instance.itemInventory[i];
+
+                    if (item != null)
+                    {
+                        int productIndex = Inventory.Instance.itemIndex[i];
+
+                        if (productIndex >= 0 && productIndex < item.itemTemplate.Length)
+                        {
+                            PRODUCTS productData = item.itemTemplate[productIndex];
+                            float itemValue;
+                            string rawValue = productData.productValue.Replace("$", "").Trim();
+
+                            if (float.TryParse(rawValue, out itemValue))
+                            {
+                                totalValue += itemValue;
+                                displayText += $"{i + 1}. {productData.productName} ${rawValue}\n";
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                inventoryDisplay.text = displayText;
+                if (total != null)
+                {
+
+                    total.text = $"Total Value: ${totalValue:F2}";
+                }
+            }
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            voucher.SetActive(false);
+        }
+    }
 }
